1b4fd4df39418544a61438051884ee3803961fbe
Casey Harding <casey.r.harding@gmail.com>
2022-06-13 13:39:16 -0400

    Final solution in place.

--- src/components/Playground.js
+++ src/components/Playground.js
@@ -13,16 +13,66 @@ import React, { useState } from 'react';
 
 function Playground(props) {
   const [count, setCount] = useState(0);
-  
+  const [spinnerOn, setSpinnerOn] = useState(false);
+  const [error, setError] = useState("");
+  const [weapon, setWeapon] = useState("");
+  const [winMessage, setWinMessage] = useState("");
+
+  const updateCount = (amount) => {
+    if (count + amount < 0) {
+      setError("Can't go below 0 ya chump!");
+      return;
+    } else if (count + amount > 10) {
+      setError("Can't go above 10 ya chump!");
+      return;
+    }
+    setCount(count + amount);
+    setError("");
+  }
+
+  const weaponChoice = (choice) => {
+    const weapons = ["rock", "paper", "scissors"];
+    const compChoice = weapons[Math.floor(Math.random() * 3)];
+    if (choice === "paper" && compChoice === "rock") {
+      setWinMessage("YOU WON!");
+    } else if (choice === "rock" && compChoice === "scissors") {
+      setWinMessage("YOU WON!");
+    } else if (choice === "scissors" && compChoice === "paper") {
+      setWinMessage("You WON!");
+    } else {
+      setWinMessage("SUCKER YOU LOST!")
+    }
+  }
+
+  if (spinnerOn) {
+    return (
+      <div className="container">
+        <h3>The spinner is {spinnerOn ? "ON" : "OFF"}</h3>
+        <button onClick={() => setSpinnerOn(false)}>Toggle Spinning Icon</button>
+      </div>
+    )
+  }
+
   return (
     <div className="container">
       <h1>HELLOOOOOOOO WORLD!</h1>
+      {error && <h2>{error}</h2>}
       <h3>The count is: {count}</h3>
-      <button onClick={() => setCount(count + 1)}>Increment Count!</button>
+      <button onClick={() => updateCount(1)}>Increment Count!</button>
+      <button onClick={() => updateCount(-1)}>Decrement Count!</button>
+      <h3>The spinner is {spinnerOn ? "ON" : "OFF"}</h3>
+      <button onClick={() => setSpinnerOn(true)}>Toggle Spinning Icon</button>
+      {winMessage && <h3>{winMessage}</h3>}
+      {weapon ? <h3>The current weapon is: {weapon}</h3> : <h3>Pick a weapon ya chump!</h3>}
+      <button onClick={() => weaponChoice("rock")}>Pick ROCK</button>
+      <button onClick={() => weaponChoice("paper")}>Pick PAPER</button>
+      <button onClick={() => weaponChoice("scissors")}>Pick SCISSORS</button>
     </div>
   )
 }
 
 // NEVER, EVER, EVER, EVER, EVER, EVER MUTATE STATE DIRECTLY
+// WAT?! is state?............................
+// data that lives in the component itself
 
 export default Playground;
